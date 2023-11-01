9631029330ab58c0304b2ed2a9cfee6db833c203
LLPROG <lucalobianco@hotmail.it>
2023-11-01 13:52:48 +0100

    try

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,17 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: '200.html', // may differ from host to host,
+			precompress: false,
+			strict: true
+		}),
 		alias: {
 			'@components': path.resolve('./src/lib/components'),
 			'@lib': path.resolve('./src/lib')
