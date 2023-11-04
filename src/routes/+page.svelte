88b87ce1acd4815323301989938f99aa5be8f2b4
LLPROG <lucalobianco@hotmail.it>
2023-11-05 00:23:19 +0100

    try fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,15 @@
 <script>
 	import LogoBigSheep from '@components/ui/LogoBigSheep.svelte';
 	import LogoMain from '@components/ui/LogoMain.svelte';
+	import { base } from '$app/paths';
 </script>
 
 <div class="w-full h-full flex flex-col items-center justify-center gap-2">
-	<a data-sveltekit-preload-data="tap" href="/home" class="z-50 flex flex-col gap-2 items-center">
+	<a
+		data-sveltekit-preload-data="tap"
+		href={`${base}/home`}
+		class="z-50 flex flex-col gap-2 items-center"
+	>
 		<LogoBigSheep />
 		<LogoMain />
 	</a>
