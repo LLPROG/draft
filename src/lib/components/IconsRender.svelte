a886b3cb9ffda2b88b784199df058b63d14c6cce
LLPROG <lucalobianco@hotmail.it>
2023-11-18 10:10:37 +0100

    added icons navbar

--- src/lib/components/IconsRender.svelte
+++ src/lib/components/IconsRender.svelte
@@ -3,8 +3,10 @@
 	import ArrowIconBlack from './ui/ArrowIconBlack.svelte';
 	import LessIcon from './ui/LessIcon.svelte';
 	import PlusIcon from './ui/PlusIcon.svelte';
-	import SaveIcon from './ui/SaveIcon.svelte';
+	import ModifyIcon from './ui/ModifyIcon.svelte';
 	import StarIcon from './ui/StarIcon.svelte';
+	import SaveIcon from './ui/SaveIcon.svelte';
+	import HambIcon from './ui/HambIcon.svelte';
 
 	export let icon = 'plus';
 </script>
@@ -24,6 +26,12 @@
 {#if icon === 'less'}
 	<span class="ms-auto"><LessIcon /></span>
 {/if}
+{#if icon === 'modify'}
+	<span class="ms-auto"><ModifyIcon /></span>
+{/if}
 {#if icon === 'save'}
 	<span class="ms-auto"><SaveIcon /></span>
+{/if}
+{#if icon === 'hamb'}
+	<span class="ms-auto"><HambIcon /></span>
 {/if}
